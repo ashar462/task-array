6d9e31502c6eaafe1e620a69d68c3a4c117548d8
Muhammad Ashar <121613707+ashar462@users.noreply.github.com>
2023-10-10 00:54:47 -0700

    Add files via upload

---
+++ task12.java
@@ -0,0 +1,25 @@
+public class task12{
+    
+    public static void main(String[] args) {
+
+        String[] fruits = {"Apple", "Mango", "Banana", "Orange","Watermelon","Grapes","Pineapple","Avocado","Papaya", "Strawberry"};
+        
+        for(int i=0; i<=fruits.length; i++){
+            System.out.println("Value of index: " + fruits[i].toUpperCase());
+        }
+       
+        // System.out.println("Value of 0 index: " + fruits[0].toUpperCase());
+        // System.out.println("Value of 1 index: " + fruits[1].toUpperCase());
+        // System.out.println("Value of 2 index: " + fruits[2].toUpperCase());
+        // System.out.println("Value of 3 index: " + fruits[3].toUpperCase());
+        // System.out.println("Value of 4 index: " + fruits[4].toUpperCase());
+        // System.out.println("Value of 5 index: " + fruits[5].toUpperCase());
+        // System.out.println("Value of 6 index: " + fruits[6].toUpperCase());
+        // System.out.println("Value of 7 index: " + fruits[7].toUpperCase());
+        // System.out.println("Value of 8 index: " + fruits[8].toUpperCase());
+        // System.out.println("Value of 9 index: " + fruits[9].toUpperCase());
+
+
+        
+    }
+}
